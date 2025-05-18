015ec9493634c7c1ccd780c2314f538a4c8912f6
kewuaa <202321011111@std.uestc.edu.cn>
2025-05-18 09:09:40 +0800

    update tests

--- tests/rpc_client.cpp
+++ tests/rpc_client.cpp
@@ -8,9 +8,13 @@
 kwa::asyncio::Task<> add() {
     TINYRPC_NS::Client c;
     co_await c.connect("127.0.0.1", 12345);
-    int res;
-    co_await kwa::tinyrpc::call_func(c, "add", res, 1, 3);
-    std::cout << "1 + 3 = " << res << std::endl;
+    int res1 = co_await kwa::tinyrpc::call_func<int>(c, "add", 1, 3);
+    std::cout << "1 + 3 = " << res1 << std::endl;
+    auto value = co_await kwa::tinyrpc::call_func<int>(c, "get_value");
+    std::cout << value << std::endl;
+    co_await kwa::tinyrpc::call_func<void>(c, "hello");
+    std::string name = "kewuaa";
+    co_await kwa::tinyrpc::call_func<void>(c, "hello_to", name);
 }
 
 
--- tests/rpc_server.cpp
+++ tests/rpc_server.cpp
@@ -7,6 +7,21 @@ int add(int a, int b) {
 }
 
 
+int get_value() {
+    return 999;
+}
+
+
+void hello() {
+    std::cout << "Hello World" << std::endl;
+}
+
+
+void hello_to(const std::string& name) {
+    std::cout << "Hello " << name << std::endl;
+}
+
+
 int main() {
 #if _DEBUG
     spdlog::set_level(spdlog::level::debug);
@@ -14,5 +29,8 @@ int main() {
     auto& server = TINYRPC_NS::Server::get();
     server.init("127.0.0.1", 12345, 1024);
     TINYRPC_NS::register_func("add", add);
+    TINYRPC_NS::register_func("get_value", get_value);
+    TINYRPC_NS::register_func("hello", hello);
+    TINYRPC_NS::register_func("hello_to", hello_to);
     kwa::asyncio::run(server.run());
 }
