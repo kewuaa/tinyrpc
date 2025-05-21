be77c32a00b8f47e15282c3bc5e28b5a4dc7a12f
kewuaa <202321011111@std.uestc.edu.cn>
2025-05-21 10:33:50 +0800

    update concepts

--- include/tinyrpc/concepts.hpp
+++ include/tinyrpc/concepts.hpp
@@ -9,11 +9,8 @@ TINYRPC_NS_BEGIN(concepts)
 
 template<typename T>
 concept ProtoType = requires(T t) {
-    requires requires (const std::string& data) {
-        { t.ParseFromString(data) } -> std::same_as<bool>;
-    };
-    requires requires (const char* data, int size) {
-        { t.ParseFromArray(data) } -> std::same_as<bool>;
+    requires requires (const void* data, int size) {
+        { t.ParseFromArray(data, size) } -> std::same_as<bool>;
     };
     requires requires (std::string* data) {
         { t.SerializeToString(data) } -> std::same_as<bool>;
